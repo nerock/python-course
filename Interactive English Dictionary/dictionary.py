bb4cb4a2a1cc7ac315a8dfcae3aafd580f0e8cfb
nerock <hi@nerock.dev>
2020-06-15 23:21:55 +0200

    Format answers and return possible matches if the word is not found

--- Interactive English Dictionary/dictionary.py
+++ Interactive English Dictionary/dictionary.py
@@ -1,13 +1,25 @@
 import json
+from difflib import get_close_matches
 
 definitions = json.load(open("data.json"))
 
 def definition(word):
     word = word.lower()
     if word in definitions:
-        return definitions[word]
+        definition = definitions[word]
 
-    return "The word doesn't exist. Please check the spelling."
+        if type(definition) == list:
+            definition = " ".join(definition)
+
+        return definition
+
+    error = "Please check the spelling."
+    close_matches = get_close_matches(word, definitions.keys())
+
+    if len(close_matches) > 0:
+        error = "Did you mean any of these? " + ",".join(close_matches)
+
+    return f"The word doesn't exist. {error}"
 
 word = input("Enter word: ")
 print(definition(word))
