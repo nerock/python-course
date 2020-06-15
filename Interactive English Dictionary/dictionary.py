a0e2b567b94c48e57033d2222e6428a244db07c5
nerock <hi@nerock.dev>
2020-06-15 23:10:53 +0200

    Make sure the word is lowercase

--- Interactive English Dictionary/dictionary.py
+++ Interactive English Dictionary/dictionary.py
@@ -3,10 +3,11 @@ import json
 definitions = json.load(open("data.json"))
 
 def definition(word):
+    word = word.lower()
     if word in definitions:
         return definitions[word]
 
-    return "The word doesn't exists. Please check the spelling."
+    return "The word doesn't exist. Please check the spelling."
 
 word = input("Enter word: ")
 print(definition(word))
