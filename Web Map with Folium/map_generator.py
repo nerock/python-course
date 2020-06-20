0ede4fbf389a9ea7304f44a6940452fce1c6ca85
nerock <hi@nerock.dev>
2020-06-20 02:19:43 +0200

    Refactor into separate method

---
+++ Web Map with Folium/map_generator.py
@@ -0,0 +1,12 @@
+import folium
+
+def valencia_map():
+    map = folium.Map(location=[39.47, -0.32], zoom_start=14, tiles="Stamen Terrain")
+
+    fg = folium.FeatureGroup(name="Valencia")
+    fg.add_child(folium.Marker(location=[39.45, -0.35], popup="Ciutat de les arts i les ciències", icon=folium.Icon(color='green')))
+    map.add_child(fg)
+
+    map.save("valencia.html")
+
+valencia_map()
